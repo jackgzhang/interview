bdb84b6d8270a4ec3a54d4e52a6bc1b046567e5c
gzhang <jackgzhang@gmail.com>
2016-06-14 09:36:05 -0700

    Q001_ThreeSum - Test relative links

--- src/main/java/weivretni/leetcode/Q063_UniquePath2.java
+++ src/main/java/weivretni/leetcode/Q063_UniquePath2.java
@@ -3,89 +3,56 @@ package weivretni.leetcode;
 /**
  * <pre>
  * Follow up for "Unique Paths":
- * 
+ *
  * Now consider if some obstacles are added to the grids. How many unique paths would there be?
- * 
+ *
  * An obstacle and empty space is marked as 1 and 0 respectively in the grid.
- * 
+ *
  * For example,
  * There is one obstacle in the middle of a 3x3 grid as illustrated below.
- * 
- * [
- *   [0,0,0],
- *   [0,1,0],
+ *
+ *
  *   [0,0,0]
- * ]
+ *   [0,1,0]
+ *   [0,0,0]
+ *
  * The total number of unique paths is 2.
- * 
+ *
  * Note: m and n will be at most 100.
  * </pre>
- * 
- * @author gzhang
+ * <p>
+ * 思路: 和前一题类似,唯一的区别是如果 obstacleGrid[][] ==1,
+ * set dp[][] = false
  *
+ * Trick: 为防止 检查obstacleGrid 时出界, 应该从右下角向左上角 计算
+ *
+ *
+ *   [0,0,0],0
+ *   [0,1,0],0
+ *   [0,0,0],0
+ *    0,0,1, 0
+ *
+ * @author gzhang
  */
 public class Q063_UniquePath2 {
 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if (obstacleGrid.length <= 0) {
-            return 0;
-        } else {
-            int[][] result = new int[obstacleGrid.length][obstacleGrid[0].length];
+        int row = obstacleGrid.length + 1;
+        int column = obstacleGrid[0].length + 1;
+        int dp[][] = new int[row][column];
 
-            // Init first row
-            boolean hasObstacle = false;
-            for (int i = 0; i < obstacleGrid.length; i++) {
-                if (obstacleGrid[i][0] == 0) {
-                    if (!hasObstacle) {
-                        result[i][0] = 1;
-                    } else {
-                        result[i][0] = 0;
-                    }
+        dp[row-2][column-1] = 1;
+
+        for (int i = row - 2; i >= 0; i--) {
+            for (int j = column - 2; j >= 0; j--) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
                 } else {
-                    hasObstacle = true;
-                    result[i][0] = 0;
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
                 }
             }
-
-            // Init first column
-            hasObstacle = false;
-            for (int i = 0; i < obstacleGrid[0].length; i++) {
-                if (obstacleGrid[0][i] == 0) {
-                    if (!hasObstacle) {
-                        result[0][i] = 1;
-                    } else {
-                        result[0][i] = 0;
-                    }
-                } else {
-                    hasObstacle = true;
-                    result[0][i] = 0;
-                }
-            }
-
-            // Now start
-            for (int i = 1; i < obstacleGrid.length; i++) {
-                for (int j = 1; j < obstacleGrid[0].length; j++) {
-                    if (obstacleGrid[i][j] == 1) {
-                        result[i][j] = 0;
-                    } else if (obstacleGrid[i - 1][j] == 1 && obstacleGrid[i][j - 1] == 1) {
-                        result[i][j] = 0;
-                    } else {
-                        int aResult = 0;
-                        if (obstacleGrid[i - 1][j] != 1) {
-                            aResult += result[i - 1][j];
-                        }
-
-                        if (obstacleGrid[i][j - 1] != 1) {
-                            aResult += result[i][j - 1];
-                        }
-
-                        result[i][j] = aResult;
-                    }
-                }
-            }
-
-            // result
-            return result[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
         }
+
+        return dp[0][0];
     }
 }
