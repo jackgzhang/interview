66d09b3c479b79e8b61bf6ce71054a945f12e3bc
gzhang <jackgzhang@gmail.com>
2016-06-15 16:41:16 -0700

    Q007_Balanced Tree

--- src/main/java/weivretni/leetcode/Q007_BalancedTree.java
+++ src/main/java/weivretni/leetcode/Q007_BalancedTree.java
@@ -14,14 +14,11 @@ import weivretni.TreeNode;
  */
 public class Q007_BalancedTree {
 
-    private int getHeight(TreeNode n) {
-        if (n == null) {
-            return 0;
-        }
-
-        return Math.max(getHeight(n.left), getHeight(n.right)) + 1;
-    }
-
+    // ======= 解法一 ======== //
+    /**
+     *  Top-down
+     *  O(n^2),  O(n) stack space
+     */
     public boolean isBalanced(TreeNode root) {
         if (root == null)
             return true;
@@ -32,5 +29,39 @@ public class Q007_BalancedTree {
             return false;
 
         return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getHeight(TreeNode n) {
+        if (n == null) {
+            return 0;
+        }
+
+        return Math.max(getHeight(n.left), getHeight(n.right)) + 1;
+    }
+
+    // ======= 解法二 ======== //
+    /**
+     *  Bottom-up
+     *  O(n^2),  O(n) stack space
+     *
+     *  The problem of 解法一 is, it re-travel the child to get
+     *  the getHeight() and isBalanced(). We can merge these 2 calls to one.
+     *  Which is use the getHeight() to return -1 if it is not balanced.
+     */
+    public boolean isBalanced2(TreeNode root) {
+        return root == null || getHeight2(root) != -1;
+    }
+
+
+    private int getHeight2(TreeNode n) {
+        if (n == null) return 0;
+        int left = getHeight2(n.left);
+        if (left == -1) return -1;
+
+        int right = getHeight2(n.right);
+        if (right == -1) return -1;
+
+        return (Math.abs(left - right) <= 1) ? Math.max(left, right) + 1: -1;
+
     }
 }
--- src/test/java/weivretni/leetcode/Q007_BalancedTree_Test.java
+++ src/test/java/weivretni/leetcode/Q007_BalancedTree_Test.java
@@ -1,18 +1,19 @@
 package weivretni.leetcode;//package weivretni.leetcode;
-//
-//import junit.framework.Assert;
-//
-//import org.junit.Test;
-//
-//import weivretni.BaseTest;
-//
-//public class Q007_BalancedTree_Test extends BaseTest {
-//
-//    private Q007_BalancedTree source = new Q007_BalancedTree();
-//
-//    @Test
-//    public void test() {
-//        Assert.assertEquals(true, source.isBalanced(this.getTreeNode1()));
-//        Assert.assertEquals(false, source.isBalanced(this.getTreeNode2()));
-//    }
-//}
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import weivretni.BaseTest;
+
+public class Q007_BalancedTree_Test extends BaseTest {
+
+    private Q007_BalancedTree source = new Q007_BalancedTree();
+
+    @Test
+    public void test() {
+        Assert.assertEquals(true, source.isBalanced2(this.buildTree(new String[]{"1"})));
+        Assert.assertEquals(source.isBalanced2(this.getTreeNode1()), source.isBalanced(this.getTreeNode1()));
+        Assert.assertEquals(source.isBalanced2(this.getTreeNode2()), source.isBalanced(this.getTreeNode2()));
+    }
+}
