4622ae7e4bddd6cd78d16241759521621b294f68
gzhang <jackgzhang@gmail.com>
2016-06-16 11:32:10 -0700

    Q001_ThreeSum - Test relative links

--- src/main/java/weivretni/leetcode/Q109_ConvertSortedListToBinarySearchTree.java
+++ src/main/java/weivretni/leetcode/Q109_ConvertSortedListToBinarySearchTree.java
@@ -1,22 +1,46 @@
 package weivretni.leetcode;//package weivretni.leetcode;
-//
-//import weivretni.ListNode;
-//import weivretni.TreeNode;
-//
-///**
-// * Given a singly linked list where elements are sorted in ascending order,
-// *
-// * convert it to a height balanced BST.
-// *
-// * Best Solution: bottom-up
-// *
-// * @author gzhang
-// *
-// */
-//public class Q109_ConvertSortedListToBinarySearchTree {
-//
-//    public TreeNode sortedListToBST(ListNode head) {
-//ss
-//    }
-//
-//}
+
+import weivretni.ListNode;
+import weivretni.TreeNode;
+
+/**
+ * Given a singly linked list where elements are sorted in ascending order,
+ * <p>
+ * convert it to a height balanced BST.
+ * <p>
+ * Best Solution: bottom-up
+ *
+ * @author gzhang
+ */
+public class Q109_ConvertSortedListToBinarySearchTree {
+
+    private ListNode listNode;
+
+    public TreeNode sortedListToBST(ListNode head) {
+        int length = 0;
+        ListNode node = head;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+
+        listNode = head;
+        return convert(0, length - 1);
+    }
+
+
+    private TreeNode convert(int start, int end) {
+        if (start > end) return null;
+
+        int mid = (start + end) / 2;
+
+        TreeNode left = convert(start, mid - 1);
+        TreeNode parent = new TreeNode(listNode.val);
+        parent.left = left;
+
+        listNode = listNode.next;
+        parent.right = convert(mid + 1, end);
+
+        return parent;
+    }
+}
--- src/test/java/weivretni/leetcode/Q109_ConvertSortedListToBinarySearchTree_Test.java
+++ src/test/java/weivretni/leetcode/Q109_ConvertSortedListToBinarySearchTree_Test.java
@@ -1,20 +1,23 @@
 package weivretni.leetcode;//package weivretni.leetcode;
-//
-//import org.junit.Test;
-//
-//import weivretni.BaseTest;
-//import weivretni.TreeNode;
-//
-//public class Q109_ConvertSortedListToBinarySearchTree_Test extends BaseTest {
-//
-//    private Q109_ConvertSortedListToBinarySearchTree source = new Q109_ConvertSortedListToBinarySearchTree();
-//
-//    @Test
-//    public void test() {
-//
-//        TreeNode answers = source.sortedListToBST(this.getListNode(new int[] { 0 }));
-//
-//        System.out.println(answers);
-//
-//    }
-//}
+
+import org.junit.Test;
+import weivretni.BaseTest;
+import weivretni.ListNode;
+
+public class Q109_ConvertSortedListToBinarySearchTree_Test extends BaseTest {
+
+    private Q109_ConvertSortedListToBinarySearchTree source2 = new Q109_ConvertSortedListToBinarySearchTree();
+    private Q108_ConvertSortedArrayToBinarySearchTree source1 = new Q108_ConvertSortedArrayToBinarySearchTree();
+
+    @Test
+    public void test() {
+        int[] array = new int[]{1,2,3,4,5};
+        ListNode list = this.buildListNode(array);
+        assertEquals(source1.sortedArrayToBST(array), source2.sortedListToBST(list));
+
+        array = new int[]{1,2,3,4,5,6,7,8};
+        list = this.buildListNode(array);
+        assertEquals(source1.sortedArrayToBST(array), source2.sortedListToBST(list));
+
+    }
+}
