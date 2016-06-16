08444e33d2254e87727d4d9ca3b58681313f2fad
gzhang <jackgzhang@gmail.com>
2016-06-16 11:43:00 -0700

    Q001_ThreeSum - Test relative links

--- src/main/java/weivretni/leetcode/Q124_BinaryTreeMaximumPathSum.java
+++ src/main/java/weivretni/leetcode/Q124_BinaryTreeMaximumPathSum.java
@@ -5,51 +5,40 @@ import weivretni.TreeNode;
 /**
  * <pre>
  * Given a binary tree, find the maximum path sum.
- * 
+ *
  * 返回树中任意两点路径的最大值。只要两点间有路径联通就可以。
- * 
+ *
  * The path may start and end at any node in the tree.
- * 
+ *
  * For example:
  * Given the below binary tree,
- * 
+ *
  *        1
  *       / \
  *      2   3
- *      
+ *
  * Return 6.
  * </pre>
- * 
+ *
  * @author gzhang
- * 
  */
 public class Q124_BinaryTreeMaximumPathSum {
 
     private int max = Integer.MIN_VALUE;
 
     private int helper(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
+        if (root == null) return 0;
 
         int left = helper(root.left);
         int right = helper(root.right);
 
 
         //用 全局 变量  保存 最大
-        int tmpMax = left + right + root.val;
-        if (tmpMax > max) {
-            max = tmpMax;
-        }
+        max = Math.max(left + right + root.val, max);
 
-        // 返回值 是 either  左边  或 右边 , 但是要是 》 0. 因为 可以 不选 左边 或 右边
-        int subSum = (root.val + left > root.val + right) ? root.val + left : root.val + right;
-        
-        if (subSum > 0) {
-            return subSum;
-        }else{
-            return 0;
-        }
+        // 如果结果<=0, 那么就返回0, 因为对root没有贡献
+        int subSum = root.val + Math.max(left, right);
+        return subSum > 0 ? subSum : 0;
     }
 
     public int maxPathSum(TreeNode root) {
--- src/test/java/weivretni/leetcode/Q124_BinaryTreeMaximumPathSum_Test.java
+++ src/test/java/weivretni/leetcode/Q124_BinaryTreeMaximumPathSum_Test.java
@@ -1,35 +1,17 @@
 package weivretni.leetcode;
 
+import junit.framework.Assert;
 import org.junit.Test;
+import weivretni.BaseTest;
 import weivretni.TreeNode;
 
-public class Q124_BinaryTreeMaximumPathSum_Test {
+public class Q124_BinaryTreeMaximumPathSum_Test extends BaseTest {
 
     private Q124_BinaryTreeMaximumPathSum source = new Q124_BinaryTreeMaximumPathSum();
-    
+
     @Test
     public void test() {
-        /**
-         * <pre>
-         *      3
-         *   2      5
-         * 1     4     6
-         * </pre>
-         */
-        TreeNode root = new TreeNode(3);
-        TreeNode left2 = new TreeNode(2);
-        TreeNode left1 = new TreeNode(1);
-        TreeNode left4 = new TreeNode(4);
-        TreeNode right5 = new TreeNode(5);
-        TreeNode right6 = new TreeNode(6);
-
-        root.left = left2;
-        root.right = right5;
-        left2.left = left1;
-        right5.right = right6;
-        right5.left = left4;
-
-        int res = source.maxPathSum(root);
-        System.out.println(res);
+        TreeNode treeNode = this.buildTree(new String[]{"-5", "2", "3", "-1", "4"});
+        Assert.assertEquals(6, source.maxPathSum(treeNode));
     }
 }
